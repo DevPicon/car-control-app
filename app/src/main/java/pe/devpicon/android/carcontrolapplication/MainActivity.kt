0de408831a69ebfb8d118dd7a2a5726b62130bd2
Armando <picondev@gmail.com>
2020-10-24 21:01:54 -0300

    link bottom navigation view

--- app/src/main/java/pe/devpicon/android/carcontrolapplication/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/carcontrolapplication/MainActivity.kt
@@ -15,6 +15,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainViewBinding.root)
 
         configRecyclerView()
+        configButtons()
+    }
+
+    private fun configButtons() {
+
+        with(mainViewBinding) {
+            bnvMenu.setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_item_summary -> {
+                        motionMain.transitionToState(R.id.summary)
+                        true
+                    }
+                    R.id.menu_item_bonnet -> {
+                        motionMain.transitionToState(R.id.bonnet)
+                        true
+                    }
+                    R.id.menu_item_wheels -> {
+                        motionMain.transitionToState(R.id.wheels)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+
+
     }
 
     private fun configRecyclerView() {
